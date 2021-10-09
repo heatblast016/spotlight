fde749f8044791250e50776b62ac51280e5b27d6
Daniel Dickman <ddickman@college.harvard.edu>
2021-10-09 19:35:51 -0400

    RoomLayout tested

--- python/src/RoomLayout.py
+++ python/src/RoomLayout.py
@@ -2,15 +2,18 @@ import csv
 
 class RoomLayout:
     dimensions = None
+    camera = None
     lights = []
 
-    def __init__(FileName):
+    def __init__(self, FileName):
         with open(FileName, 'r') as input:
             reader = csv.reader(input, delimiter=' ')
             
             row1 = next(reader)
-            dimensions = (float(row1[0]), float(row1[1]))
+            self.dimensions = (float(row1[0]), float(row1[1]))
+            row2 = next(reader)
+            self.camera = (float(row2[0]), float(row2[1]))
             
             for line in reader:
-                lights.append(float(line[0]), float(line[1]))
+                self.lights.append((float(line[0]), float(line[1])))
 

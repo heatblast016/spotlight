74427a17f0cf55e92b5f2484908c0e9593ae6f96
Daniel Dickman <ddickman@college.harvard.edu>
2021-10-10 01:04:38 -0400

    cleaned up some stuff

--- python/src/RoomLayout.py
+++ python/src/RoomLayout.py
@@ -3,11 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class RoomLayout:
-    dimensions = None
-    camera = None
-    lights = []
-    humans = []
-    graph = None
+    dimensions = None   # (x, z)
+    camera = None       # (x, z) - points upwards
+    lights = []         # [(x1, z1, current power, goal power), ...]
+    people = []         # [(x1, z1), ...]
 
     def __init__(self, FileName):
         with open(FileName, 'r') as input:
@@ -19,26 +18,28 @@ class RoomLayout:
             self.camera = (float(row2[0]), float(row2[1]))
             
             for line in reader:
-                self.lights.append((float(line[0]), float(line[1])))
+                self.lights.append((float(line[0]), float(line[1]), 0, 0))
     
-    def PlaceHuman(self, locs):
-        self.lights = []
+    def RefreshPeople(self, locs):
+        self.people = []
         for i in range(len(locs)):
-            self.humans.append((self.camera[0] + locs[i][0],
+            self.people.append((self.camera[0] + locs[i][0],
                                 self.camera[1] + locs[i][1]))
-    
+
     def DrawRoom(self):
-        fig, ax = plt.subplots()  # Create a figure containing a single axes.
-      
+        # set up the graph      
+        fig, ax = plt.subplots()
         plt.xlim([0, self.dimensions[0]])
         plt.ylim([0, self.dimensions[1]])
         plt.gca().set_aspect('equal', adjustable='box')
         
-        
-        for person in self.humans:
+        # add points for people
+        for person in self.people:
             ax.plot(person[0], person[1], 'r+')
         
+        # 
 
         plt.draw()
-        plt.pause(3)
-        plt.clf()
+        # move these to main:
+            # plt.pause(3)
+            # plt.clf()

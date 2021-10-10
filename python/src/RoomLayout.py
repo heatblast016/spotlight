48be744d3bb29abd63253e2d7930d40334e23980
Daniel Dickman <ddickman@college.harvard.edu>
2021-10-10 02:01:41 -0400

    about to mess with display a loooooot

--- python/src/RoomLayout.py
+++ python/src/RoomLayout.py
@@ -1,12 +1,17 @@
 import csv
 import matplotlib.pyplot as plt
+plt.ion()
+import matplotlib.patches as patches
 import numpy as np
 
 class RoomLayout:
     dimensions = None   # (x, z)
     camera = None       # (x, z) - points upwards
-    lights = []         # [(x1, z1, current power, goal power), ...]
+    lights = []         # [[x1, z1, current power (? / 1.0), goal power (? / 1.0)], ...]
     people = []         # [(x1, z1), ...]
+
+    MAX_LIGHT_ALPHA = 0.4
+    MAX_LIGHT_RAD = 5
 
     def __init__(self, FileName):
         with open(FileName, 'r') as input:
@@ -18,7 +23,7 @@ class RoomLayout:
             self.camera = (float(row2[0]), float(row2[1]))
             
             for line in reader:
-                self.lights.append((float(line[0]), float(line[1]), 0, 0))
+                self.lights.append([float(line[0]), float(line[1]), 0, 1])
     
     def RefreshPeople(self, locs):
         self.people = []
@@ -26,20 +31,36 @@ class RoomLayout:
             self.people.append((self.camera[0] + locs[i][0],
                                 self.camera[1] + locs[i][1]))
 
-    def DrawRoom(self):
+    def RefreshRoom(self, ax, PeopleLocs):
         # set up the graph      
-        fig, ax = plt.subplots()
+        
         plt.xlim([0, self.dimensions[0]])
         plt.ylim([0, self.dimensions[1]])
         plt.gca().set_aspect('equal', adjustable='box')
         
         # add points for people
+        self.RefreshPeople(PeopleLocs)
         for person in self.people:
             ax.plot(person[0], person[1], 'r+')
         
-        # 
+        # TODO: calculate goal powers
 
-        plt.draw()
+        # draw the light circles
+        for light in self.lights:
+            # adjust light power towards goal
+            if light[2] < light[3]:
+                light[2] += 0.1
+            elif light[2] > light[3]:
+                light[2] -= 0.1
+            
+            # draw the light circle
+            circle = patches.Circle((light[0], light[1]),
+                                     light[2] * RoomLayout.MAX_LIGHT_RAD,
+                                     alpha = light[2] * RoomLayout.MAX_LIGHT_ALPHA,
+                                     color = 'y')
+            ax.add_patch(circle)
+
+        # plt.draw()
         # move these to main:
             # plt.pause(3)
             # plt.clf()

97485ac3c620d9175374a5c6c55cd60d9a4a519f
Daniel Dickman <ddickman@college.harvard.edu>
2021-10-10 09:13:00 -0400

    pushing test code for use in main

---
+++ python/src/test_input1.py
@@ -0,0 +1,35 @@
+from RoomLayout import RoomLayout
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+room = RoomLayout(sys.argv[1])
+print('dimensions: ', room.dimensions)
+print('camera:     ', room.camera)
+print('lights:     ', room.lights)
+
+humans = [(3, 4), (-2, 8)]  # my fake GetHumanLocs()
+print('humans:     ', room.people)
+
+# set up the graph    
+"""
+fig, ax = plt.subplots()
+
+plt.ion()
+for i in range(50):
+    room.RefreshRoom(ax, humans)
+    plt.xlim([0, room.dimensions[0]])
+    plt.ylim([0, room.dimensions[1]])
+    plt.gca().set_aspect('equal', adjustable='box')
+    fig.canvas.draw()
+    time.sleep(0.5)
+    fig.canvas.flush_events()
+    plt.cla()
+"""
+
+human1 = [(-5, 3)]
+while True:
+    room.RefreshRoom(human1, 1)
+    human1 = [(human1[0][0] + 0.04, human1[0][1] + 0.02)]
+    # print(human1)
+    

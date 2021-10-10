14c59b2a70b4f85f6609f49f50fe33f765d4d169
Daniel Dickman <ddickman@college.harvard.edu>
2021-10-09 23:14:07 -0400

    I'm going back to matplotlib

--- python/src/DrawRoom.py
+++ python/src/DrawRoom.py
@@ -3,38 +3,12 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import time
-
-def gauplot(centers, radiuses, xr=None, yr=None):
-    nx, ny = 1000.,1000.
-    xgrid, ygrid = np.mgrid[xr[0]:xr[1]:(xr[1]-xr[0])/nx,yr[0]:yr[1]:(yr[1]-yr[0])/ny]
-    im = xgrid*0 + np.nan
-    xs = np.array([np.nan])
-    ys = np.array([np.nan])
-    fis = np.concatenate((np.linspace(-np.pi,np.pi,100), [np.nan]) )
-    cmap = plt.cm.gray
-    cmap = plt.cm.get_cmap("gray").copy()
-    cmap.set_bad('white')
-    thresh = 3
-    for curcen,currad in zip(centers,radiuses):
-        curim=(((xgrid-curcen[0])**2+(ygrid-curcen[1])**2)**.5)/currad*thresh
-        im[curim<thresh]=np.exp(-.5*curim**2)[curim<thresh]
-        xs = np.append(xs, curcen[0] + currad * np.cos(fis))
-        ys = np.append(ys, curcen[1] + currad * np.sin(fis))
-    plt.imshow(im.T, cmap=cmap, extent=xr+yr, origin="lower")
-    # plt.plot(xs, ys, 'r-')
-    plt.show()
-
-
-gauplot([(0,0), (2,3), (5,1), (6,7), (6.1, 6.1)], [.3, .4, .5, 1, .4], [-1,10], [-1,10])
-gauplot([(6.1, 6.1)], [.4], [-1,10], [-1,10])
-
-"""
 import cv2
-import numpy as np
 
 class DrawRoom:
     def __init__(self, room, imshow):
+        graph = [10 * ][][3]
+        graph.fill(255)
 
 image = np.zeros((100, 200, 3), np.uint8)
 cv2.imshow('image window', image)
@@ -48,4 +22,3 @@ for i in range(200):
     
 cv2.imshow('image window', image)
 cv2.waitKey(0)
-"""
--- python/src/RoomLayout.py
+++ python/src/RoomLayout.py
@@ -1,10 +1,13 @@
 import csv
+import numpy as np
+import cv2
 
 class RoomLayout:
     dimensions = None
     camera = None
     lights = []
     humans = []
+    graph = None
 
     def __init__(self, FileName):
         with open(FileName, 'r') as input:
@@ -17,9 +20,38 @@ class RoomLayout:
             
             for line in reader:
                 self.lights.append((float(line[0]), float(line[1])))
+
+        GraphWidth = int(10 * (self.dimensions[0] + 2))
+        GraphHeight = int(10 * (self.dimensions[1] + 2))
+        
+        self.graph = np.zeros((GraphHeight, GraphWidth, 3), np.uint8)
+        self.graph.fill(255)
+
+        for i in range(9, GraphWidth - 9):
+            self.graph[9][i] = [0, 0, 0]
+            self.graph[GraphHeight - 9][i] = [0, 0, 0]
+
+        for i in range(9, GraphHeight - 9):
+            self.graph[i][9] = [0, 0, 0]
+            self.graph[i][GraphWidth - 9] = [0, 0, 0]
+
+
     
     def PlaceHuman(self, locs):
         self.lights = []
         for i in range(len(locs)):
             self.humans.append((self.camera[0] + locs[i][0],
                                 self.camera[1] + locs[i][1]))
+    
+    def DrawRoom(self, LightScalars):
+
+        
+        
+        # scale the image
+        scale_percent = 220 # percent of original size
+        width = int(self.graph.shape[1] * scale_percent / 100)
+        height = int(self.graph.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        resized = cv2.resize(self.graph, dim, interpolation = cv2.INTER_AREA)
+        cv2.imshow('room', resized)
+        cv2.waitKey(0)

4218ef9cc9477a09625f9f741e78aaff4c574437
Daniel Dickman <ddickman@college.harvard.edu>
2021-10-09 20:06:07 -0400

    added PlaceHuman to RoomLayout class

--- python/src/RoomLayout.py
+++ python/src/RoomLayout.py
@@ -4,6 +4,7 @@ class RoomLayout:
     dimensions = None
     camera = None
     lights = []
+    humans = []
 
     def __init__(self, FileName):
         with open(FileName, 'r') as input:
@@ -16,4 +17,9 @@ class RoomLayout:
             
             for line in reader:
                 self.lights.append((float(line[0]), float(line[1])))
-
+    
+    def PlaceHuman(self, locs):
+        self.lights = []
+        for i in range(len(locs)):
+            self.humans.append((self.camera[0] + locs[i][0],
+                                self.camera[1] + locs[i][1]))
